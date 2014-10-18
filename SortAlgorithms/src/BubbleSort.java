e2c86dd037ec1e5a60851c0255fe8c76cbb10214
joeyD <joey.duffin@gmail.com>
2014-10-18 08:14:03 +0100

    selection sort added

--- SortAlgorithms/src/BubbleSort.java
+++ SortAlgorithms/src/BubbleSort.java
@@ -7,17 +7,28 @@ public class BubbleSort {
 		int[] myInts = new int[30];
 		
 		// first populate array randomly and display integers.
-		populate(myInts);
+		randomize(myInts);
 		//display the array
-		JOptionPane.showMessageDialog(null, buildMessage(myInts));
+		JOptionPane.showMessageDialog(null, buildMessage(myInts, "Random Ints"));
 		// now the bubble sort,
 		bubbleSort(myInts);
 		//display the array
-		JOptionPane.showMessageDialog(null, buildMessage(myInts));
+		JOptionPane.showMessageDialog(null, buildMessage(myInts, "Bubble Sorted Ints"));
+		
+		//re-random
+		randomize(myInts);
+		JOptionPane.showMessageDialog(null, buildMessage(myInts, "Random Ints"));
+		// now the bubble sort,
+		selectionSort(myInts);
+		//display the array
+		JOptionPane.showMessageDialog(null, buildMessage(myInts, "Selection Sorted Ints"));
+		
+		
+		
 
 	}
 	//bubbleSort!
-	public static void bubbleSort(int[] myInts) {
+	private static void bubbleSort(int[] myInts) {
 
 		boolean Swapped = true;
 		while (Swapped) {
@@ -30,23 +41,50 @@ public class BubbleSort {
 			}
 		}
 	}
+	
+	//selectionSort!
+	private static void selectionSort(int[] myInts) {
+		int minI;
+		int i;
+		int j;
+		
+		for (i = 0; i < myInts.length - 1; i++) {
+			minI = i;
+			for (j = i+1; j < myInts.length; j++) {
+				if (myInts[j] < myInts[minI]) {
+					minI = j;
+				}
+			}
+			if (minI != i) {
+				swap(myInts, i, minI);
+			}
+		}
+	}
 
 	//swaps the (i)th and (i+1)th elements in the array.
-	public static void swap(int[] myInts, int i) {
+	private static void swap(int[] myInts, int i) {
 		int temp = myInts[i];
 		myInts[i] = myInts[i + 1];
 		myInts[i + 1] = temp;
 	}
+	
+	//swaps the (i)th and j(th) elements in an array
+	private static void swap(int[] myInts, int i, int j){
+		int temp = myInts[i];
+		myInts[i] = myInts[j];
+		myInts[j] = temp;
+	}
 
-	public static StringBuilder buildMessage(int[] myInts){
+	private static StringBuilder buildMessage(int[] myInts, String sortType){
 		StringBuilder message = new StringBuilder();
+		message.append(sortType + ":\n");
 		for (int i = 0; i < myInts.length; i++) {
 			message.append(myInts[i] + " ");
 		}
 		return message;
 	}
 	
-	public static void populate(int[] myInts) {
+	private static void randomize(int[] myInts) {
 
 		Random rand = new Random();
 		for (int i = 0; i < myInts.length; i++) {
